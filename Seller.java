403f9794d825a177d2fcffb57f75c08aea30b944
namman-273 <25021902@vnu.edu.vn>
2026-04-04 22:13:18 +0700

    Delete Seller.java

--- Seller.java
+++
@@ -1,8 +0,0 @@
-public class Seller extends User{
-    public Seller(String id, String username) { super(id, username); }
-
-    @Override
-    public void displayInfo() {
-        System.out.println("[Seller] ID: " + id + ", Name: " + username);
-    }
-}
