40da19381c279555879b67aee5e7dbd892741dd1
namman-273 <25021902@vnu.edu.vn>
2026-04-04 22:10:13 +0700

    Delete Admin.java

--- Admin.java
+++
@@ -1,8 +0,0 @@
-public class Admin extends User{
-    public Admin(String id, String username) { super(id, username); }
-
-    @Override
-    public void displayInfo() {
-        System.out.println("[Admin] ID: " + id + ", Name: " + username);
-    }
-}
