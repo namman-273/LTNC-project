1412d54c6c14ed1228305b619103477f52bd58c0
namman-273 <25021902@vnu.edu.vn>
2026-04-24 15:22:26 +0700

    Update AuctionService.java

--- src/main/java/com/auction/service/AuctionService.java
+++ src/main/java/com/auction/service/AuctionService.java
@@ -9,14 +9,18 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class AuctionService implements Serializable {
-    //phong ngua th doi ten bien,java se tinh ra id moi co the k khop voi id file cu dc serialize len
+    // phong ngua th doi ten bien,java se tinh ra id moi co the k khop voi id file
+    // cu dc serialize len
     private static final long serialVersionUID = 1L;
-    // Dùng ConcurrentHashMap để đảm bảo an toàn khi nhiều ClientHandler cùng truy cập
-    private  final Map<String, Auction> auctions=new ConcurrentHashMap<>();
+    // Dùng ConcurrentHashMap để đảm bảo an toàn khi nhiều ClientHandler cùng truy
+    // cập
+    private final Map<String, Auction> auctions = new ConcurrentHashMap<>();
     // Singleton pattern,Instance (Sử dụng từ khóa volatile để an toàn đa luồng)
     private static volatile AuctionService instance;
-    private AuctionService(){//khoi tao du lieu mau/
+
+    private AuctionService() {// khoi tao du lieu mau/
     }
+
     public static AuctionService getInstance() {
         if (instance == null) {
             synchronized (AuctionService.class) {
@@ -27,14 +31,17 @@ public class AuctionService implements Serializable {
         }
         return instance;
     }
-    public void addAuction(Auction auction){
-        if (auction!=null){
-            auctions.put(auction.getId(),auction);
+
+    public void addAuction(Auction auction) {
+        if (auction != null) {
+            auctions.put(auction.getId(), auction);
         }
     }
+
     public Auction getAuctionById(String id) {
         return auctions.get(id);
     }
+
     /**
      * Lấy toàn bộ danh sách phiên đấu giá.
      * FE sẽ cần cái này để hiển thị danh mục hàng hóa.
@@ -42,14 +49,20 @@ public class AuctionService implements Serializable {
     public Collection<Auction> getAllAuctions() {
         return auctions.values();
     }
+
     public Item getItemInAuction(String auctionId) {
         Auction a = auctions.get(auctionId);
         return (a != null) ? a.getItem() : null;
     }
-    //Hỗ trọ Serialization
-    // Sau khi load từ file, ta cần gán lại instance để cả hệ thống dùng chung bản vừa load
+
+    // Hỗ trọ Serialization
+    // Sau khi load từ file, ta cần gán lại instance để cả hệ thống dùng chung bản
+    // vừa load
     public static void setInstance(AuctionService loadedInstance) {
-        //chúng ta đang ở trong một hàm static (hàm của lớp, không phải của đối tượng cụ thể). Lúc này, chưa có đối tượng instance nào tồn tại để mà khóa, nên ta phải dùng chính cái "khuôn mẫu" của lớp đó (AuctionService.class) làm điểm tựa để khóa.
+        // chúng ta đang ở trong một hàm static (hàm của lớp, không phải của đối tượng
+        // cụ thể). Lúc này, chưa có đối tượng instance nào tồn tại để mà khóa, nên ta
+        // phải dùng chính cái "khuôn mẫu" của lớp đó (AuctionService.class) làm điểm
+        // tựa để khóa.
         synchronized (AuctionService.class) {
             instance = loadedInstance;
         }
