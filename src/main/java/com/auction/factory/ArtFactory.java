bdedab07200f97ec8e2cc0fa56d2ce2e3e342821
namman-273 <25021902@vnu.edu.vn>
2026-04-25 18:30:16 +0700

    Update ArtFactory.java

--- src/main/java/com/auction/factory/ArtFactory.java
+++ src/main/java/com/auction/factory/ArtFactory.java
@@ -3,6 +3,9 @@ package com.auction.factory;
 import com.auction.model.*;
 
 public class ArtFactory extends ItemFactory {
+
+    public int a_b=0;
+
     @Override
     public Item create(String id, String name, double price) {
         return new Art(id, name, price); // Gọi đến class Art.java trong model của bạn
