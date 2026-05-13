cf13ba8f527bbcef08dfa101ea3adbf1e67bd80c
phanminhquan22122000-a11y <phanminhquan22122000@gmail.com>
2026-05-13 09:45:02 +0700

    .

--- src/test/java/com/auction/model/AuctionStateMachineTest.java
+++ src/test/java/com/auction/model/AuctionStateMachineTest.java
@@ -28,7 +28,7 @@ public class AuctionStateMachineTest {
         Field umField = UserManager.class.getDeclaredField("instance");
         umField.setAccessible(true);
         umField.set(null, null);
-        UserManager.getInstance().register("alice", "pw", "BIDDER");
+        UserManager.getInstance().register("alice", "pw", "BIDDER", null);
         bidder1 = (Bidder) UserManager.getInstance().findUserByUsername("alice");
         bidder1.addBalance(10_000_000.0);
     }
