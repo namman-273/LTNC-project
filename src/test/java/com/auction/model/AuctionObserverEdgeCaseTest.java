1daa4fde6a512c37bda7105dff4e98a4bd2dca12
phanminhquan22122000-a11y <phanminhquan22122000@gmail.com>
2026-05-13 09:43:58 +0700

    .

--- src/test/java/com/auction/model/AuctionObserverEdgeCaseTest.java
+++ src/test/java/com/auction/model/AuctionObserverEdgeCaseTest.java
@@ -33,7 +33,7 @@ public class AuctionObserverEdgeCaseTest {
         Field umField = UserManager.class.getDeclaredField("instance");
         umField.setAccessible(true);
         umField.set(null, null);
-        UserManager.getInstance().register("alice", "pw", "BIDDER");
+        UserManager.getInstance().register("alice", "pw", "BIDDER", null);
         bidder1 = (Bidder) UserManager.getInstance().findUserByUsername("alice");
         bidder1.addBalance(10_000_000.0);
 
