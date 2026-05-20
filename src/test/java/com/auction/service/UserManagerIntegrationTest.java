9726ecacfd804e700c7091b9f1138b1955e1fddd
phanminhquan22122000-a11y <phanminhquan22122000@gmail.com>
2026-05-20 22:17:22 +0700

    .

--- src/test/java/com/auction/service/UserManagerIntegrationTest.java
+++ src/test/java/com/auction/service/UserManagerIntegrationTest.java
@@ -18,9 +18,8 @@ public class UserManagerIntegrationTest {
  
     @BeforeEach
     void setUp() throws Exception {
-        Field field = UserManager.class.getDeclaredField("instance");
-        field.setAccessible(true);
-        field.set(null, null);
+        // Holder idiom: clear users via setUsers() thay vì reflection.
+        UserManager.getInstance().setUsers(new java.util.HashMap<>());
         manager = UserManager.getInstance();
     }
  
